4d2054216966eaac9ed7001e07148019cb816b14
xxxily <974278171@qq.com>
2026-03-24 19:51:47 +0800

    docs: Update docs

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -37,6 +37,29 @@ export default {
     // sidebar: 'auto',
     sidebar: zhSidebar,
     socialLinks: [{ icon: 'github', link: 'https://github.com/xxxily/hello-ai' }],
+    // search: {
+    //   provider: 'local',
+    //   options: {
+    //     locales: {
+    //       zh: {
+    //         translations: {
+    //           button: {
+    //             buttonText: '搜索文档',
+    //             buttonAriaLabel: '搜索文档'
+    //           },
+    //           modal: {
+    //             noResultsText: '无法找到相关结果',
+    //             resetButtonTitle: '清除查询条件',
+    //             footer: {
+    //               selectText: '选择',
+    //               navigateText: '切换'
+    //             }
+    //           }
+    //         }
+    //       }
+    //     }
+    //   }
+    // }
     // displayAllHeaders: true,
     // sidebarDepth: 5,
     // lastUpdated: 'Last Updated',
